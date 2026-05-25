108d4f2e66dd8b275b87804670c938fdb67620b2
Levo <levoyanturier@gmail.com>
2026-05-25 11:28:57 +0200

    Add speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from "./App";
 import "./App.css";
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
