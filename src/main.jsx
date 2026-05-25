2971b370557d6c0169a6f2d9a0b25451e9e0e0bd
Levo <levoyanturier@gmail.com>
2026-05-25 11:30:32 +0200

    Fix speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <App />
       <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 );
